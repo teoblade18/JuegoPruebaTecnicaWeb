ec28461ffcbcaf6ab63b54231a402306ce679609
T30 <mateo.a.e8@gmail.com>
2024-09-04 17:49:48 -0500

    arreglar verficar ganador

--- juego-prueba-tecnica/src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ juego-prueba-tecnica/src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgModel } from '@angular/forms';
@@ -6,7 +7,7 @@ import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-piedra-papel-tijera',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './piedra-papel-tijera.component.html',
   styleUrl: './piedra-papel-tijera.component.css'
 })
@@ -23,7 +24,7 @@ export class PiedraPapelTijeraComponent {
   private idJugador1 = 0;
   private idJugador2 = 0;
   private idPartida = 0;
-  private apiUrl = 'http://localhost:5000/api/PartidaService';
+  private apiUrl = 'https://localhost:7174/api/PartidaService';
 
   constructor(private http: HttpClient) {}
 
@@ -33,10 +34,10 @@ export class PiedraPapelTijeraComponent {
       { nombre: this.jugador2Nombre }
     ];
 
-    this.http.post(`${this.apiUrl}/IniciarPartida`, jugadores).subscribe((partida: any) => {
-      this.idPartida = partida.idPartida;
-      this.idJugador1 = partida.jugadores[0].idJugador;
-      this.idJugador2 = partida.jugadores[1].idJugador;
+    this.http.post(`${this.apiUrl}/IniciarPartida`, jugadores).subscribe((respuestaPartida: any) => {
+      this.idPartida = respuestaPartida.partida.idPartida;
+      this.idJugador1 = respuestaPartida.jugadores[0].idJugador;
+      this.idJugador2 = respuestaPartida.jugadores[1].idJugador;
       this.juegoIniciado = true;
     });
   }
@@ -63,12 +64,16 @@ export class PiedraPapelTijeraComponent {
       idPartida: this.idPartida
     };
 
-    this.http.post(`${this.apiUrl}/RegistrarMovimiento`, movimiento1).subscribe();
-    this.http.post(`${this.apiUrl}/RegistrarMovimiento`, movimiento2).subscribe(() => {
-      this.http.get(`${this.apiUrl}/VerificarGanador/${this.idPartida}`).subscribe((resultado: any) => {
-        this.resultadoRonda = resultado;
-        this.verificarGanador();
-        this.rondaTerminada = true;
+    this.http.post(`${this.apiUrl}/RegistrarMovimiento`, movimiento1, { responseType: 'text' }).subscribe();
+    this.http.post(`${this.apiUrl}/RegistrarMovimiento`, movimiento2, { responseType: 'text' }).subscribe((resultadoRonda: any) => {
+
+      this.rondaTerminada = true;
+      this.resultadoRonda = resultadoRonda;
+      
+      this.http.get(`${this.apiUrl}/VerificarGanador/${this.idPartida}`, {responseType: 'text' }).subscribe((resultado: any) => {
+        if(resultado != "Aún no hay un ganador. Continúa jugando."){
+          this.ganador = resultado;
+        }
       });
     });
   }
@@ -79,12 +84,6 @@ export class PiedraPapelTijeraComponent {
     this.movimientoJugador2 = '';
     this.rondaTerminada = false;
     this.resultadoRonda = '';
-  }
-
-  verificarGanador() {
-    if (this.resultadoRonda.includes('ha ganado la partida!')) {
-      this.ganador = this.resultadoRonda.split(' ')[1];
-    }
   }
 
   reiniciarJuego() {
